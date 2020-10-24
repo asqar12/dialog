7f18f58c94e18d5452f140de083dfea03c02ac1b
asqar12 <easqar12@gmail.com>
2020-10-24 17:44:35 +0300

    I will change the word

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ import(
 	"fmt"
 )
 func main() {
-	fmt.Println("A--dialoge bitwin T and A ")
-	fmt.Println("T--Hello Asqar")
-	fmt.Println("A--Hi Tolmosbek")
-	fmt.Println("T--How are you")
-	fmt.Println("A--I'm very will thanks and you")
-	fmt.Println("T--How old are you now")
-	fmt.Println("A--I am naintin yars old")
-	fmt.Println(" T--What is your flowers")
-	fmt.Println("A--play geymes listing mp3")
-	fmt.Println("T--it is great you're succew")
-	fmt.Println("A--Yes it is true")
+	fmt.Println("A--Step 1. Create a Repository ")
+	fmt.Println("T--What is GitHub?")
+	fmt.Println("A-Step 1. Create a Repository")
+	fmt.Println("T--To create a new repository")
+	fmt.Println("A--Step 2. Create a Branch")
+	fmt.Println("T--Step 3. Make and commit changes")
+	fmt.Println("A--Step 4. Open a Pull Request")
+	fmt.Println(" Step 5. Merge your Pull Request")
+	fmt.Println("A--Celebrate!")
+	fmt.Println("By completing this tutorial, you’ve learned to create a project and make a pull request on GitHub!")
+	fmt.Println(`Created an open source repositor  
+	By completing this tutorial, you’ve learned to 
+	
+	
+	
+	
+	create a project
+	 and make a pull request on GitHub `)
 }
