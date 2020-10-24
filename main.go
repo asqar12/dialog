80e8b76926c4a68d6a6171d474f018e2fd1a05b8
asqar12 <easqar12@gmail.com>
2020-10-24 06:48:53 +0300

    dialog

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+import(
+	"fmt"
+)
+func main() {
+	fmt.Println("A--dialoge bitwin T and A ")
+	fmt.Println("T--Hello Asqar")
+	fmt.Println("A--Hi Tolmosbek")
+	fmt.Println("T--How are you")
+	fmt.Println("A--I'm very will thanks and you")
+	fmt.Println("T--How old are you now")
+	fmt.Println("A--I am naintin yars old")
+	fmt.Println(" T--What is your flowers")
+	fmt.Println("A--play geymes listing mp3")
+	fmt.Println("T--it is great you're succew")
+	fmt.Println("A--Yes it is true")
+}
